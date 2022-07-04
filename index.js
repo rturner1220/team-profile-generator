2b968069a4a06bdc6804a89c289b43bbfe93ca45
Rosa Turner <rmturner1220@gmail.com>
2022-07-04 17:02:51 -0400

    Team Profile is Successfully

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const path = require('path');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Inter = require('./lib/Inter');
 const generatePage = require('./src/template');
+
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const outputPath = path.join(DIST_DIR, 'index.html');
 const teamArr = [];
 const idArr = [];
 
@@ -70,32 +74,190 @@ function init() {
             }
         ])
         .then(answers => {
+          const manager = new Manager(answers.name, answers.Id, answers.email, answers.number);
+          teamArr.push(manager);
+          idArr.push(answers.Id);
+          addTeam();
+        });
+      }
 
-        })
+    // addTeam function after created manager
+    function addTeam() {
+        inquirer.prompt([
+          {
+            type: 'list',
+            name: 'teamChoice',
+            message: 'What would you like to add?',
+            choices: ['Engineer', 'Intern', 'End profile']
+          }
+       ]).then(userChoice => {
+         switch (userChoice.teamChoice) {
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Intern':
+                addInter();
+                break;
+            default:
+                generateHTML();
+         }
+       });
+    }
 
+ // create a engineer choice
+ function addEngineer () {
+    inquirer.prompt([
+        {
+         type: 'input',
+         name: 'name',
+         message: "What is the engineer's name? (Required)",
+         validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter the name!');
+              return false;
+            }
+          }
+        },
+        {
+         type: 'input',
+         name: 'Id',
+         message: "What is the engineer's Id? (Required)",
+         validate: idInput => {
+            if (idInput) {
+              return true;
+            } else {
+              console.log('Please enter the ID!');
+              return false;
+            }
+          }
+        },
+        {
+         type: 'input',
+         name: 'email',
+         message: "What is the engineer's email? (Required)",
+         validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+              console.log('Please enter the email!');
+              return false;
+            }
+          }
+        },
+        {
+         type: 'input',
+         name: 'github',
+         message: "What is the engineer's GitHub username? (Required)",
+         validate: githubInput => {
+            if (githubInput) {
+              return true;
+            } else {
+              console.log('Please enter the GitHub username!');
+              return false;
+            }
+          }
+        }
+     ]).then(answers => {
+         const engineer = new Engineer(answers.name, answers.Id, answers.email, answers.github);
+         teamArr.push(engineer);
+         idArr.push(answers.engineerId);
+         addTeam();
+     });
+  }
 
+// create a intern choice
+function addInter () {
+    inquirer.prompt([
+        {
+         type: 'input',
+         name: 'name',
+         message: "What is the intern's name? (Required)",
+         validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter the name!');
+              return false;
+            }
+          }
+        },
+        {
+         type: 'input',
+         name: 'Id',
+         message: "What is the intern's Id? (Required)",
+         validate: idInput => {
+            if (idInput) {
+              return true;
+            } else {
+              console.log('Please enter the ID!');
+              return false;
+            }
+          }
+        },
+        {
+         type: 'input',
+         name: 'email',
+         message: "What is the intern's email? (Required)",
+         validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+              console.log('Please enter the email!');
+              return false;
+            }
+          }
+        },
+        {
+         type: 'input',
+         name: 'school',
+         message: "What is the intern's school name? (Required)",
+         validate: schoolInput => {
+            if (schoolInput) {
+              return true;
+            } else {
+              console.log('Please enter the school name!');
+              return false;
+            }
+          }
+        }
+     ]).then(answers => {
+         const inter = new Inter(answers.name, answers.Id, answers.email, answers.school);
+         teamArr.push(inter);
+         idArr.push(answers.interId);
+         addTeam();
+     });
+  }
 
-
-
-
-
+ // create a function to write index file
+  function generateHTML() {
+    if (!fs.existsSync(DIST_DIR)) {
+        fs.mkdirSync(DIST_DIR)
+    }
+    console.log("Congrats, Your Team Profile is Successfully!!!");
+    fs.writeFileSync(outputPath, generatePage(teamArr));
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      createManager ();
 }
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/template.js
+++ src/template.js
@@ -1,7 +1,44 @@
 // Export to generate HTML page
+// create team profile
+
+module.exports = team => {
+  return`
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,700;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+
+  <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 page-title">
+                <h1 class="text-center">Team Profile</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="main-section col-12 d-flex justify-content-center">
+                ${generatePage(team)}
+            </div>
+        </div>
+    </div>
+  </body>
+  </html>  
+    `;
+};
 
 // create team profile
-const teamProfile = team => {
+const generatePage = team => {
 
     // create manager profile
     const managerProfile = manager => {
@@ -13,9 +50,9 @@ const teamProfile = team => {
         </div>
         <div class="card-body bg-light">
             <ul class="list-group text-dark">
-                <li class="list-group-item">ID:${manager.getId()}</li>
-                <li class="list-group-item">Email:<a href="mailto:${manager.getEmail()}'>${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: <a href="tel:${manager.getofficeNumber()}">${manager.getofficeNumber()}</a></li>
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: <a href="tel:${manager.getOfficeNumber()}">${manager.getOfficeNumber()}</a></li>
             </ul>
         </div>
     </div>
@@ -32,9 +69,9 @@ const engineerProfile = engineer => {
     </div>
     <div class="card-body bg-light">
         <ul class="list-group text-dark">
-            <li class="list-group-item">ID:${engineer.getId()}</li>
-            <li class="list-group-item">Email:<a href="mailto:${engineer.getEmail()}'>${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGitHub()}" target="_blank" rel="noopener noreferrer">${engineer.getGitHub()}</a></li>
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
         </ul>
     </div>
 </div>
@@ -51,9 +88,9 @@ const interProfile = inter => {
     </div>
     <div class="card-body bg-light">
         <ul class="list-group text-dark">
-            <li class="list-group-item">ID:${inter.getId()}</li>
-            <li class="list-group-item">Email:<a href="mailto:${inter.getEmail()}'>${inter.getEmail()}</a></li>
-            <li class="list-group-item">School:${intern.getSchool()}</li>
+            <li class="list-group-item">ID: ${inter.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${inter.getEmail()}">${inter.getEmail()}</a></li>
+            <li class="list-group-item">School: ${inter.getSchool()}</li>
         </ul>
     </div>
 </div>
@@ -67,18 +104,18 @@ html.push(team
     .map(manager => managerProfile(manager))
  );
 
- 
 html.push(team
     .filter(employee => employee.getRole() === 'Engineer')
     .map(engineer => engineerProfile(engineer))
+    .join('')
  );
-
  
 html.push(team
     .filter(employee => employee.getRole() === 'Inter')
     .map(inter => interProfile(inter))
+    .join('')
  );
 
- return html.join("");
+ return html.join('');
 
 }
