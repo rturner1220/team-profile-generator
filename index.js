b46aef186d0d8cc996cefc3a9877523ddec08aea
Rosa Turner <rmturner1220@gmail.com>
2022-07-04 14:27:46 -0400

    created function manager

--- index.js
+++ index.js
@@ -6,3 +6,96 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Inter = require('./lib/Inter');
 const generatePage = require('./src/template');
+const teamArr = [];
+const idArr = [];
+
+// start application
+function init() {
+
+    // create a manager when application starts
+    function createManager () {
+        console.log("Start building a team profile");
+        inquirer.prompt([
+            {
+             type: 'input',
+             name: 'name',
+             message: "What is the manager's name? (Required)",
+             validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the name!');
+                  return false;
+                }
+              }
+            },
+            {
+             type: 'input',
+             name: 'Id',
+             message: "What is the manager's ID? (Required)",
+             validate: idInput => {
+                if (idInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the ID!');
+                  return false;
+                }
+              }
+            },
+            {
+             type: 'input',
+             name: 'email',
+             message: "What is the manager's email? (Required)",
+             validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the email!');
+                  return false;
+                }
+              }
+            },
+            {
+             type: 'input',
+             name: 'number',
+             message: "What is the manager's office number? (Required)",
+             validate: numberInput => {
+                if (numberInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the office number!');
+                  return false;
+                }
+              }
+            }
+        ])
+        .then(answers => {
+
+        })
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
